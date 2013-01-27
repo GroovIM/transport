2d666f6fadabcdb2011f45d667e6d3942176376b
Luba Tang <lubatang@gmail.com>
2013-01-27 13:39:40 +0800

    Add file templates.

---
+++ templates/header.h
@@ -0,0 +1,21 @@
+//===- header.h -----------------------------------------------------------===//
+//
+//
+//===----------------------------------------------------------------------===//
+#ifndef GROOVIM_${CLASS_NAME}_H
+#define GROOVIM_${CLASS_NAME}_H
+
+namespace groovim {
+
+/** \class ${class_name}
+ *  \brief ${brief}
+ */
+class ${class_name}
+{
+
+};
+
+} // namespace of groovim
+
+#endif
+
